a69156d90e1833d518c9f7a401d5f2fc97ab6a76
Kuznetcova Arina <kuznetsova.av@phystech.edu>
2019-11-14 15:52:04 +0300

    14.11.2019 15:52

--- solar_input.py
+++ solar_input.py
@@ -19,7 +19,7 @@ def read_space_objects_data_from_file(input_filename):
             if len(line.strip()) == 0 or line[0] == '#':
                 continue  # пустые строки и строки-комментарии пропускаем
             object_type = line.split()[0].lower()
-            if object_type == "star":  # FIXME: DONE the same for planet
+            if object_type == "star":  # DONE the same for planet
                 star = Star()
                 parse_star_parameters(line, star)
                 objects.append(star)
@@ -95,8 +95,8 @@ def write_space_objects_data_to_file(output_filename, space_objects):
     """
     with open(output_filename, 'w') as out_file:
         for obj in space_objects:
-            print(out_file, "%s %d %s %f" % ('1', 2, '3', 4.5))
-            # FIXME: should store real values
+            print(out_file, "%s %d %s %f %f %f %f %f" % (obj.type, obj.R, obj.color, obj.m, obj.x, obj.y, obj.Vx, obj.Vy))
+            # DONE: should store real values
 
 # FIXME: хорошо бы ещё сделать функцию, сохранающую статистику в заданный файл...
 
